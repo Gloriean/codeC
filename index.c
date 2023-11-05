e061987249afa71e69e5b2794f05624bbdaa7b09
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-05 08:58:53 +0100

    cModule

--- index.c
+++ index.c
@@ -1,6 +1,21 @@
-
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void){
-  printf("Hello World! \n");
+  double a = 3459858555.00;
+  double b = 26.47477474;
+  double c = 338383833838.2772772;
+  
+  int num;
+  num = 4;
+
+  printf("%lf\n", a);
+
+  printf("%lf\n", b);
+
+  printf("%lf\n", c);
+
+  printf("%i\n", num);
+
+  return 0;
 }
