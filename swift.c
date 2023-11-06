85cef783b264723d78591fc8dac9bc5adac6b202
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-06 10:16:34 +0100

    cModule

---
+++ swift.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int var = 2;
+
+    switch (var) {
+    case 1:
+        printf("Case 1 is executed");
+        break;
+    case 2:
+        printf("Case 2 is executed");
+        break;
+    }
+
+    return 0;
+}
