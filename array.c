7a7b3be8cf7d8f6764bdd007c448651e78cca7d9
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-11 21:17:58 +0100

    Array in C

--- array.c
+++ array.c
@@ -6,6 +6,22 @@ int main(void){
     //two times the previous one.
     //Start the array at 1
 
-printf("Size: ");
+int myNumbers[6];
+   myNumbers[0] = 34;
+   myNumbers[1] = 22;
+   myNumbers[2] = 12;
+   myNumbers[3] = 0;
+   myNumbers[4] = 9;
+   myNumbers[5] = 45;
+
+    printf("Num 1: %d\n\n", (myNumbers[0]));
+    printf("Num 2: %d\n\n", (myNumbers[1]));
+    printf("Num 3: %d\n\n", (myNumbers[2]));
+    printf("Num 4: %d\n\n", (myNumbers[3]));
+    printf("Num 5: %d\n\n", (myNumbers[4]));
+    printf("Num 6: %d\n\n", (myNumbers[5]));
+
 
 }
+
+
