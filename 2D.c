184dbf07ac990a5b1042739ee68a8bb23895bf06
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-13 22:26:58 +0100

    2 Dimensional Array

--- 2D.c
+++ 2D.c
@@ -2,7 +2,18 @@
 
 int main(void){
    
+ int matrix[2][3] = {{2, 3, 4}, {7, 8, 9}};
+    printf("%d\n\n", matrix[0][0]);
+    printf("%d\n\n", matrix[0][1]);
+    printf("%d\n\n", matrix[0][2]);
+    printf("%d\n\n", matrix[1][0]);
+    printf("%d\n\n", matrix[1][1]);
+    printf("%d\n\n", matrix[1][2]);
 
+    matrix[0][0] = 56;
+    printf("%d\n\n", matrix[0][0]);
+
+    
     //looping through a multidimensional array
     int matrix[2][2] = {{22, 4}, {6, 9}};
     int i, j;
