79b3500e9780b4a9f3dfe779216502c697e3a1f8
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-15 22:12:38 +0100

    Math Function in C

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("Output: %f\n", sqrt(16));
+    printf("Output: %f\n", sqrt(9.8));
+    printf("Output: %f\n", ceil(6.9));
+    printf("Output: %f\n", floor(7.8));
+    printf("Output: %f\n", pow(2,5));
+    printf("Output: %f\n", fabs(99.089));
+    printf("Output: %f\n", exp(7));
+    printf("Output: %f\n", tan(100));
+
+}
