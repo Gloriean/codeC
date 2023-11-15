ce9008f2be58fe85b5095155ecb90c5032b93675
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-15 22:11:42 +0100

    Function in C

---
+++ func1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//function declaration
+int Function(int x, int y){
+    return x + y;
+}
+
+//The main method
+int main(){
+    //call the function
+    int result = Function(6, 8);
+    printf("Result is: %d\n", result);
+    return 0;
+}
+
+
+    
